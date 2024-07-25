7775710df184870b0e2de244b30b44ccb6c455e9
naidelii <naidelii@qq.com>
2024-07-25 16:59:54 +0800

    feat：分类新增、编辑、删除功能 refactor：格式化代码

--- src/views/modules/sys/menu.vue
+++ src/views/modules/sys/menu.vue
@@ -90,6 +90,7 @@
         </template>
       </el-table-column>
     </el-table>
+    
     <!-- 弹窗, 新增 / 修改 -->
     <add-or-update
       v-if="addOrUpdateVisible"
@@ -100,72 +101,70 @@
 </template>
 
 <script>
-import AddOrUpdate from "./menu-add-or-update";
-import { treeDataTranslate } from "@/utils";
+import AddOrUpdate from './menu-add-or-update'
+import { treeDataTranslate } from '@/utils'
 export default {
-  data() {
+  data () {
     return {
       dataForm: {},
       dataList: [],
       dataListLoading: false,
-      addOrUpdateVisible: false,
-    };
+      addOrUpdateVisible: false
+    }
   },
   components: {
-    AddOrUpdate,
+    AddOrUpdate
   },
-  activated() {
-    this.getDataList();
+  activated () {
+    this.getDataList()
   },
   methods: {
     // 获取数据列表
-    getDataList() {
-      this.dataListLoading = true;
+    getDataList () {
+      this.dataListLoading = true
       this.$http({
-        url: this.$http.adornUrl("/sys/permission/list"),
-        method: "get",
-        params: this.$http.adornParams(),
+        url: this.$http.adornUrl('/sys/permission/list'),
+        method: 'get',
+        params: this.$http.adornParams()
       }).then(({ data }) => {
-        this.dataList = treeDataTranslate(data.data);
-        this.dataListLoading = false;
-      });
+        this.dataList = treeDataTranslate(data.data)
+        this.dataListLoading = false
+      })
     },
     // 新增 / 修改
-    addOrUpdateHandle(id) {
-      this.addOrUpdateVisible = true;
+    addOrUpdateHandle (id) {
+      this.addOrUpdateVisible = true
       this.$nextTick(() => {
-        this.$refs.addOrUpdate.init(id);
-      });
+        this.$refs.addOrUpdate.init(id)
+      })
     },
     // 删除
-    deleteHandle(id) {
-      this.$confirm(`确定对[id=${id}]进行[删除]操作?`, "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          this.$http({
-            url: this.$http.adornUrl(`/sys/permission/delete/${id}`),
-            method: "get",
-            data: this.$http.adornData(),
-          }).then(({ data }) => {
-            if (data && data.code === 200) {
-              this.$message({
-                message: "操作成功",
-                type: "success",
-                duration: 1500,
-                onClose: () => {
-                  this.getDataList();
-                },
-              });
-            } else {
-              this.$message.error(data.msg);
-            }
-          });
+    deleteHandle (id) {
+      this.$confirm(`确定对[id=${id}]进行[删除]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl(`/sys/permission/delete/${id}`),
+          method: 'get',
+          data: this.$http.adornData()
+        }).then(({ data }) => {
+          if (data && data.code === 200) {
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.getDataList()
+              }
+            })
+          } else {
+            this.$message.error(data.msg)
+          }
         })
-        .catch(() => {});
-    },
-  },
-};
+      }).catch(() => {})
+    }
+  }
+}
 </script>
